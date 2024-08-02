48025e1b645ce8548c646801ab70b1a2217183e0
Alejandro Antonio Villazón Gutiérrez <alejandro.villazon@usm.cl>
2024-08-02 19:16:23 -0400

    comments in english

--- 1_gen_variogram_maps.R
+++ 1_gen_variogram_maps.R
@@ -6,10 +6,10 @@ np <- import("numpy")
 ruta_al_archivo_npy <- ".../X_train.npy" # X_test.npy # fields_sst.npy
 datos_python <- np$load(ruta_al_archivo_npy)
 
-N <- dim(datos_python)[1] # cantidad de simulaciones
-size_field <- dim(datos_python)[2] # largo/ancho campo
+N <- dim(datos_python)[1] # number of fields
+size_field <- dim(datos_python)[2]
 
-cutoff <- size_field * sqrt(2) / 3 # valor por defecto en variogram()
+cutoff <- size_field * sqrt(2) / 3 # default value in variogram()
 
 maps_vario <- array(NA, dim = c(N, 13, 13))
 field_xyz <- expand.grid(x = 1:16, y = 1:16)
@@ -17,14 +17,14 @@ field_xyz <- expand.grid(x = 1:16, y = 1:16)
 time <- system.time(
 for (i in 1:N){
   Z <- datos_python[i,,]
-  field_xyz$Z <- rev(c(apply(Z, 1, rev))) # Vectorizamos en orden correcto
+  field_xyz$Z <- rev(c(apply(Z, 1, rev))) # vectorize in the correct order
   field <- SpatialPointsDataFrame(coords = field_xyz[, c("x", "y")],
                                   data = field_xyz)
   variograma <- variogram(Z~1, data=field, cutoff=cutoff, width=cutoff*2/12.8, map=TRUE)
-  maps_vario[i,,] <- matrix(variograma$map$var1, nrow=13, byrow=TRUE) # Guardamos el mapa como matriz
+  maps_vario[i,,] <- matrix(variograma$map$var1, nrow=13, byrow=TRUE) # Save map as matrix
 }
 )
-print(time) 
+print(time) # in seconds
 # TRAIN SET
 # user  system elapsed 
 # 3546.28    3.00 3605.16 
